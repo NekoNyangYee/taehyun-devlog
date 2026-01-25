27f8628515b9257bf055d5383e59180581e0cb5a
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2026-01-25 23:11:52 +0900

    fix: update sitemap siteUrl setting

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://taehyun-blog-renewal.vercel.app", // 본인 도메인
-  generateRobotsTxt: true, // robots.txt 자동 생성 여부
-  sitemapSize: 7000, // URL이 많으면 여러 개 파일로 쪼개줌
+  siteUrl: "https://taehyun-devlog.vercel.app", // 본인 도메인
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
 };
