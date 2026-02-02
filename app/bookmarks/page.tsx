a9a67912835a76b8af0eccad2626b3f3da72a90d
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2026-02-02 11:09:18 +0900

    feat: 봇이 크롤링 할 수 있게 SSR로 변경

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,5 +1,10 @@
 // app/bookmark/page.tsx
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
+
+export const metadata: Metadata = {
+  title: "북마크 | TaeHyun's Devlog",
+};
 
 // ✅ 클라이언트에서만 실행되도록 withSessionCheck 적용된 컴포넌트를 불러옴
 const BookMarkPageWithSession = dynamic(
